a8e5f3146702f73036a2ab422ce38860e1e327fb
Elielson68 <elielsonbr.com@gmail.com>
2023-05-19 00:50:12 -0300

    Add funcionalidade de renomear e deletar item do histórico Criando novo document pro item do story Criando popup para renomear item Criando popup para deletar item

--- Assets/Scripts/HistoryController.cs
+++ Assets/Scripts/HistoryController.cs
@@ -5,6 +5,9 @@ using UnityEngine.UIElements;
 
 public class HistoryController : MonoBehaviour
 {
+    public VisualTreeAsset ItemStory;
+    public VisualTreeAsset EditNamePopup;
+    public VisualTreeAsset DeleteItemPopup;
     private List<ClassificationData> _classifications;
 
     private const string HistoryContent = "history-content";
@@ -38,20 +41,116 @@ public class HistoryController : MonoBehaviour
             FindObjectOfType<OptionController>().ChangeScreen(Screens.Initial);
         });
 
+        CreateStoryItens();
+    }
+
+    private void CreateStoryItens()
+    {
         foreach(string dir in Directory.GetDirectories(ClassificationFolder.SaveDataFolder+"/Reports/"))
         {
-            Button reportFolder = new Button();
+            VisualElement baseVE = ItemStory.Instantiate();
+            HistoryContentVE.Add(baseVE);
+
+            Label reportFolder = baseVE.Q<Label>("item");
             reportFolder.text = (new DirectoryInfo(dir)).Name;
-            HistoryContentVE.Add(reportFolder);
-            reportFolder.focusable = true;
             reportFolder.RegisterCallback<ClickEvent>(SetReportData);
-            reportFolder.AddToClassList(ReportTitleStyle);
+
+            ConfigureEditButton(baseVE, reportFolder.text);
+            ConfigureDeleteButton(baseVE, reportFolder.text);
+        }
+    }
+
+    private void ConfigureEditButton(VisualElement baseVE, string reportFolderName)
+    {
+        Button edit = baseVE.Q<Button>("edit");
+        edit.RegisterCallback<ClickEvent>(evt => {
+            Debug.Log("Clicou no Edit");
+            VisualElement popup = EditNamePopup.Instantiate();
+            _document.rootVisualElement.Add(popup);
+            ConfigureEditPopup(popup, reportFolderName);
+        });
+    }
+
+    private void ConfigureDeleteButton(VisualElement baseVE, string reportFolderName)
+    {
+        Button delete = baseVE.Q<Button>("delete");
+        delete.RegisterCallback<ClickEvent>(evt => {
+            Debug.Log("Clicou no Delete");
+            VisualElement popup = DeleteItemPopup.Instantiate();
+            _document.rootVisualElement.Add(popup);
+            ConfigureDeletePopup(popup, reportFolderName);
+        });
+    }
+
+    private void ConfigureEditPopup(VisualElement popup, string oldName)
+    {
+        TextField name = popup.Q<TextField>("name");
+        name.SetValueWithoutNotify(oldName);
+        Button cancel = popup.Q<Button>("cancel");
+        cancel.RegisterCallback<ClickEvent>(evt => 
+        {
+            Debug.Log("Cancelou a renomeação");
+            _document.rootVisualElement.Remove(popup);
+        });
+
+        Button save = popup.Q<Button>("save");
+        save.RegisterCallback<ClickEvent>(evt =>
+        {  
+            Debug.Log($"Salvou o novo nome! {name.value}");
+            ChangeNameItem(oldName, name.value);
+            _document.rootVisualElement.Remove(popup);
+        });
+    }
+
+    private void ConfigureDeletePopup(VisualElement popup, string name)
+    {
+        Button cancel = popup.Q<Button>("cancel");
+        cancel.RegisterCallback<ClickEvent>(evt => 
+        {
+            Debug.Log("Cancelou a renomeação");
+            _document.rootVisualElement.Remove(popup);
+        });
+
+        Button save = popup.Q<Button>("save");
+        save.RegisterCallback<ClickEvent>(evt =>
+        {  
+            Debug.Log($"Vai tentar deletar o {name}");
+            DeleteItem(name);
+            _document.rootVisualElement.Remove(popup);
+        });
+    }
+
+    private void ChangeNameItem(string oldName, string newName)
+    {
+        try
+        {
+            Directory.Move(string.Format(ClassificationFolder.PathFolderFile, oldName), string.Format(ClassificationFolder.PathFolderFile, newName));
+            HistoryContentVE.Clear();
+            CreateStoryItens();
+        }
+        catch(System.Exception e)
+        {
+            Debug.Log($"Houve um erro ao tentar renomear! Message: {e.Message}\n{e.StackTrace}");
+        }
+    }
+
+    private void DeleteItem(string name)
+    {
+        try
+        {
+            Directory.Delete(string.Format(ClassificationFolder.PathFolderFile, name), true);
+            HistoryContentVE.Clear();
+            CreateStoryItens();
+        }
+        catch(System.Exception e)
+        {
+            Debug.Log($"Houve um erro ao tentar deletar! Message: {e.Message}\n{e.StackTrace}");
         }
     }
 
     private void SetReportData(ClickEvent evt)
     {
-        Button label = evt.target as Button;
+        Label label = evt.target as Label;
         foreach(var cls in _classifications)
         {
             cls.classification.ImportJson(label.text);
